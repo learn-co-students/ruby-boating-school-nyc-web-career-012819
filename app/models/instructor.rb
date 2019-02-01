397bb127d0ff1dd210b28e1b5043d41bd9bd05b1
Ethan Nam <ethan.s.nam@gmail.com>
2019-01-31 23:26:55 -0500

    Solved coding challenge

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
 class Instructor
 
+	@@all = []
+
+	def initialize(name)
+		@name = name
+	end
+
+	def find_test(student, test_name)
+		student.tests.find { |test| test.name == test_name }
+	end
+
+	def pass_student(student, test_name)
+		test = self.find_test(student, test_name)
+
+		if test != nil
+			test.status = "passed"
+		else
+			BoatingTest.new(student, test_name, "passed", self)
+		end
+	end
+
+	def fail_student(student, test_name)
+		test = self.find_test(student, test_name)
+
+		if test != nil
+			test.status = "failed"
+		else
+			BoatingTest.new(student, test_name, "failed", self)
+		end
+	end
+
+	def self.all
+		@@all
+	end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
+
+require 'pry'
 class Student
+
+	attr_reader :first_name
+
+	@@all = []
+
+	def initialize(first_name)
+		@first_name = first_name
+		@@all << self
+	end
+
+	def add_boating_test(test_name, test_status, instructor)
+		BoatingTest.new(self, test_name, test_status, instructor)
+	end
+
+	def grade_percentage
+		self.passed_tests.length.to_f / self.tests.length.to_f
+	end
+
+	def tests
+		BoatingTest.all.select { |test| test.student == self }
+	end
+
+	def passed_tests
+		self.tests.select { |test| test.test_status == "passed" }
+	end
+
+	def self.find_student(first_name)
+		@@all.find { |student| student.first_name == first_name }
+	end
+
+	def self.all
+		@@all
+	end
 
 end
