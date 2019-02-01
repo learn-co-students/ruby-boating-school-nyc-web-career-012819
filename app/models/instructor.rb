9f4a092b689b62abafa2201fb95c9edc8cb8945c
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-01-31 21:12:01 -0500

    in one hour

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,22 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+  end
+
+  def fail_student(student, test_name)
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 class Student
 
+  attr_accessor :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(name, status, instructor)
+    BoatingTest.new(self, name, status, instructor)
+  end
+
+  def self.find_student(first_name)
+    self.all.find do |student|
+      student.first_name == student.first_name
+    end
+  end
+
+  def grade_percentage
+  end
+
 end
