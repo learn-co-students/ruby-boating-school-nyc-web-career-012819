298927eba973ee94e86530b4fcda2b888ea0f8c1
akramhelil <akramhelil@gmail.com>
2019-02-24 18:25:14 -0500

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,17 @@
 class BoatingTest
+  attr_accessor :test_status, :test_name, :student
+  @@all = []
+
+  def initialize(student, test_name, test_status, instructor)
+    @student = student
+    @test_name = test_name
+    @test_status = "pending"
+    @instructor = instructor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,32 @@
 class Instructor
+  attr_reader :student, :test_status
+  @@all = []
 
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(test_student, test_name)
+    if Student.find_student(test_student) == test_student
+      BoatingTest.test_status = "passed"
+    else
+      new_student  = Student.new(test_student)
+     new_student.add_boating_test(test_name, test_status = "passed", self)
+    end
+  end
+
+  def fail_student(test_student, test_name)
+    if Student.find_student(test_student) == test_student
+      BoatingTest.test_status = "failed"
+    else
+      new_student  = Student.new(test_student)
+     new_student.add_boating_test(test_name, test_status = "failed", self)
+    end
+  end
+
+end #class ending
